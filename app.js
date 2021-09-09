a7aefbc61126ac2fb60267128271f73b601d8967
darvish2 <60345474+darvish2@users.noreply.github.com>
2021-09-10 07:06:31 +0900

    up

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var hello = require('./routes/bonjour');
+var bonjour = require('./routes/bonjour');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
